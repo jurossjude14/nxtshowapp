10dcad8faec006a180ac137f7adbdebf1b06f00e
Juross Madrid <36556961+jurossjude14@users.noreply.github.com>
2024-05-25 19:50:08 +0800

    Delete src directory

--- src/app/providers/IndexedDbQuery.js
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import Dexie from 'dexie';
-
-export const db = new Dexie('animaDB');
-db.version(1).stores({
-  animelist: '++id, mal_id, title, images, episodes, aired, status, type' // Primary key and indexed props
-});
--- src/app/providers/UseContextProvider.js
+++
@@ -1,9 +0,0 @@
-
-'use client';
-import { createContext } from 'react';
-
-
-export const Datacontext = createContext({
-    objdata: [],
-    loading: true,
-});
