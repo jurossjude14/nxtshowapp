829fd01853419322a3eb5c96602f09f5c9d50a0b
Juross Madrid <36556961+jurossjude14@users.noreply.github.com>
2024-06-01 22:41:14 +0800

    Update mail.js

--- src/lib/mail.js
+++ src/lib/mail.js
@@ -16,25 +16,56 @@ export async function sendMail({
       pass: SMTP_PASSWORD,
     },
   });
-  try {
-    const testResult = await transport.verify();
-    console.log(testResult);
-  } catch (error) {
-    console.error({ error });
-    return;
-  }
-
-  try {
-    const sendResult = await transport.sendMail({
+  // try {
+  //   const testResult = await transport.verify();
+  //   console.log(testResult);
+  // } catch (error) {
+  //   console.error({ error });
+  //   return;
+  // }
+  await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transport.verify(function (error, success) {
+          if (error) {
+              console.log(error);
+              reject(error);
+          } else {
+              console.log("Server is ready to take our messages");
+              resolve(success);
+          }
+      });
+  });
+  
+  // try {
+  //   const sendResult = await transport.sendMail({
+  //     from: SMTP_EMAIL,
+  //     to,
+  //     subject,
+  //     html: body,
+  //   });
+  //   console.log(sendResult);
+  // } catch (error) {
+  //   console.log(error);
+  // }
+await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail({
       from: SMTP_EMAIL,
       to,
       subject,
       html: body,
+    }, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
     });
-    console.log(sendResult);
-  } catch (error) {
-    console.log(error);
-  }
+});
+
+  
 }
 
 // export function compileWelcomeTemplate(name, url) {
