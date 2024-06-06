ba30bf7dd4647384f1105cc64989276d30d93517
Juross Madrid <36556961+jurossjude14@users.noreply.github.com>
2024-06-06 21:05:01 +0800

    Update route.js | Update Next Public API

--- src/app/webform/api/lead/route.js
+++ src/app/webform/api/lead/route.js
@@ -3,15 +3,15 @@
 import { NextResponse } from "next/server";
 import { senddata } from "../../components/mailaction";
 
-const API_KEY = '68821227a4ba380f5f084c86869e66b7e35fd'; // Replace with your actual API key
-const API_URL = 'https://jurosleads-d86e.restdb.io/rest/leadsdb';
+//const API_KEY = '68821227a4ba380f5f084c86869e66b7e35fd'; // Replace with your actual API key
+//const API_URL = 'https://jurosleads-d86e.restdb.io/rest/leadsdb';
 
 export async function POST(request) {
   try {
       const body = await request.json();
       const raw = JSON.stringify({...body});
       const myHeaders = new Headers();
-            myHeaders.append('x-apikey', API_KEY);
+            myHeaders.append('x-apikey', process.env.NEXT_PUBLIC_RESTDB_API_KEY);
             myHeaders.append('Content-Type', 'application/json');
     
       const requestOptions = {
@@ -21,7 +21,7 @@ export async function POST(request) {
         redirect: 'follow',
       };
 
-      const response = await fetch(API_URL, requestOptions);
+      const response = await fetch(process.env.NEXT_PUBLIC_RESTDB_API_URL, requestOptions);
       if (!response.ok) {
         throw new Error(`API request failed with status ${response.status}`);
       }
