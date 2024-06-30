6838063f90eba4d1aa2e2344a0b2725a20cc48f5
Juross Madrid <36556961+jurossjude14@users.noreply.github.com>
2024-06-30 23:02:12 +0800

    Update route.js | Added Get All Leads

--- src/app/webform/api/lead/route.js
+++ src/app/webform/api/lead/route.js
@@ -34,3 +34,30 @@ export async function POST(request) {
       console.log(error);
   }
 };
+
+export async function GET(request) {
+    try {   
+        const myHeaders = new Headers();
+        myHeaders.append('x-apikey', process.env.NEXT_PUBLIC_RESTDB_API_KEY);
+        myHeaders.append('Content-Type', 'application/json');
+
+        const requestOptions = {
+          method: 'GET',
+          headers: myHeaders,
+          redirect: 'follow'
+        };
+
+        const response = await fetch(process.env.NEXT_PUBLIC_RESTDB_API_URL, requestOptions);
+        const result = await response.json();
+
+        if (!response.ok) {
+          throw new Error(`API request failed with status ${response.status}`);
+        }
+        if (response.status === 200) {
+          console.log('Sucessfully Fetched All Data',result, response.status);
+        }
+        return NextResponse.json({ message:"Sucessfully Fetched All Data", status:response.status, data:result});    
+    } catch (error) {
+      return NextResponse.json({ message:`${error}`});
+    }
+};
